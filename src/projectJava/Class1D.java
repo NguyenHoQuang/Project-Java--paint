79538a1e3696427766340a87060202079b98562c
Nguyễn Hồ Quang <49974540+NguyenHoQuang@users.noreply.github.com>
2019-07-03 13:36:56 +0700

    Đồ án java

--- src/projectJava/Class1D.java
+++
@@ -1,5 +0,0 @@
-package projectJava;
-
-public interface Class1D extends Paint{
-	
-}
--- src/projectJava/Class2D.java
+++
@@ -1,7 +0,0 @@
-package projectJava;
-
-import java.awt.Color;
-
-public interface Class2D extends Paint{
-	   public void fill(Color color);	//Chuc nang to mau cho doi tuong 2D
-	}
